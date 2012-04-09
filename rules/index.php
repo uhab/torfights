ad5a09449ba2ab1d6c36668a12a150790b69e483
uhab <moresheth@gmail.com>
2012-04-09 21:26:55 +0000

    Rules:  adding bum fight rules

--- rules/index.php
+++ rules/index.php
@@ -7,7 +7,41 @@ include("../inc/header.php");
 
 ?>
 
-<h1>Rules</h1>
-<p>Official rules.</p>
+<h1>Tournament Rules</h1>
+
+<h2>The Bum Fights:</h2>
+
+<p>Payout:  Champion receives: 1,000,000 credits | Second place receives: 300,000 credits.</p>
+
+<p>Location:  Nar Shaddaa, Promenade cantina, Stage floor.
+
+<p>Participants must register with a member of Merenzane Gold for the tournament.</p>
+
+<p>Participants choose a companion at time of registration and that companion is locked in for the whole tournament.</p>
+
+<p>There will be a registration fee of 100,000 credits that must be collected at the time of registration.</p>
+
+<p>Each Participant will perform a roll of /roll 100 which will be used to determine bracket placement for the tournament.</p>
+
+<p>Player 1 in the bracket will be the lowest roll, Player 16 will be the highest.</p>
+
+<p>Each battle in the tournament will consist of each participant's "bum" fighting each other.  The controlling participant must order their bumn to attack the other players and then move to the assigned area.</p>
+
+<p>Once in the assigned area, the participant must not move nor activate any personal ability for the remainder of the battle.</p>
+
+<p>However, the participant MAY micro manage his bum's abilities or they can just leave it to the bum to auto use thier skills.</p>
+
+<p>If a participant does move out of the assinged area or help his bum with any action/skill, that participant will be disqualified from the tournament and the other participant will be declared victor of that battle.</p>
+
+<p>Participants must have all 4 class buffs before the battle for uniformity.  Presence stimpaks are optional.</p>
+
+<p>If the battle has not ended after 5 minutes, the battle will be stopped and a /roll 100 with the highest number becoming the victor.</p>
+
+<p>If there is any outside interference to the tournament, then the battles will be moved to another location.</p>
+
+
+
+
+
 
 <?php include("../inc/footer.php"); ?>
