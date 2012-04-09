b5a5c28a0cbc06be081b8fc28a148b2c52074504
uhab <moresheth@gmail.com>
2012-04-09 18:11:37 +0000

    Testing bracket embed: refresh bracket

--- brackets/index.php
+++ brackets/index.php
@@ -7,12 +7,7 @@ include("../inc/header.php");
 
 ?>
 
-<h1>Brackets</h1>
-<p>Tournament information.</p>
-
-<div id="livebracket">
-        
-    <iframe name="iBracket" width='805' height='725' frameborder='1' src='https://docs.google.com/spreadsheet/pub?key=0Anc7cr6pHAmWdDNuX3hMU09fcTJ3N2JwbnMtWTNuS2c&single=true&gid=0&output=html&widget=true'></iframe>
+<head>
 
     <script type = "text/javascript">
 
@@ -23,8 +18,20 @@ include("../inc/header.php");
         setInterval("refreshIframe()",30000); 
 
     </script>
+    
+</head>
+
+<body>
+
+<h1>Brackets</h1>
+<p>Tournament information.</p>
+
+<div id="livebracket">
+        
+    <iframe name="iBracket" width='805' height='725' frameborder='1' src='https://docs.google.com/spreadsheet/pub?key=0Anc7cr6pHAmWdDNuX3hMU09fcTJ3N2JwbnMtWTNuS2c&single=true&gid=0&output=html&widget=true'></iframe>
 
 </div>
 
+</body>
 
 <?php include("../inc/footer.php"); ?>
