6c7a26aaa967876a3939f92819ce43f677989f25
uhab <moresheth@gmail.com>
2012-04-09 17:09:28 +0000

    Testing bracket embed

--- brackets/index.php
+++ brackets/index.php
@@ -10,4 +10,9 @@ include("../inc/header.php");
 <h1>Brackets</h1>
 <p>Tournament information.</p>
 
+<div id="livebracket">
+    <iframe width='500' height='300' frameborder='0' src='https://docs.google.com/spreadsheet/pub?key=0Anc7cr6pHAmWdDNuX3hMU09fcTJ3N2JwbnMtWTNuS2c&single=true&gid=0&output=html&widget=true'></iframe>
+</div>
+
+
 <?php include("../inc/footer.php"); ?>
